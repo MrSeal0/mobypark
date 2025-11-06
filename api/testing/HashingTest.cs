c04eebd62c241c2f5f7e2a2065c076d98335aad4
Joris Tammes <jorisezta@gmail.com>
2025-11-06 12:41:37 +0100

    Update HashingTest.cs

--- api/testing/HashingTest.cs
+++ api/testing/HashingTest.cs
@@ -1,9 +1,26 @@
+using System.Reflection;
+
+namespace testing;
 [TestClass]
 public class HashingTest
 {
+    ArgonHasher hasher = new();
+
     [TestMethod]
-    public void TestHashPassword()
+    [DataRow("PlasticFles123")]
+    [DataRow("PlasticFles123!")]
+    public void TestHashPassword(string password)
     {
-        
+        Assert.IsFalse(hasher.HashPassword(password) == password);
+    }
+
+
+    [TestMethod]
+    [DataRow("PlasticSoep123")]
+    [DataRow("PlasticSoep123!")]
+    public void TestVerifyPassword(string password)
+    {
+        string hashedPassword = hasher.HashPassword(password);
+        Assert.IsTrue(hasher.VerifyPassword(password, hashedPassword));
     }
 }
