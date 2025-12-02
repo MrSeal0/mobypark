6f19f8a9c27790d0596212219e24760a10026e2c
Joris Tammes <jorisezta@gmail.com>
2025-12-02 10:02:29 +0100

    fix paymenttest

--- testing/PaymentTest.cs
+++ testing/PaymentTest.cs
@@ -1,21 +1,27 @@
 using System.Transactions;
 using Dapper;
 using System.Reflection;
+using System;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations.Schema;
+using API_C_.Controllers;
 
-
+namespace testing;
 [TestClass]
 public class PaymentTest : TestAcces
 {
+    public override string Table() => "Payments";
+
     [TestMethod]
     [DataRow("JorisTest", 10)]
     [DataRow("OokJoris", 12)]
-    public PaymentGetTest(string user, int am)
+    public void PaymentGetTest(string user, int am)
     {
         string sql = $"INSERT INTO {Table()} (amount, created_at, hash, initiator, \"transaction\") VALUES (@amount, @created_at, @hash, @initiator, @transaction)";
-        _con.Execute(sql, new {amount = am, created_at = datetime.Now(), hash = "idk", initiator = user, transaction = "abc123"});
+        _con.Execute(sql, new {amount = am, created_at = DateTime.Now, hash = "idk", initiator = user, transaction = "abc123"});
 
-        string sql = $"SELECT amount FROM {Table()} WHERE initiator = @Initiator";
-        int newAmount = _con.Query<int>(sql, new { Initiator = user }).ToList();
+        string sql2 = $"SELECT amount FROM {Table()} WHERE initiator = @Initiator";
+        int newAmount = _con.Execute(sql2, new { Initiator = user });
 
         Assert.IsTrue(newAmount == am);
     }
