3cae91c59ad9fc728e300d9daec95609f1ee9028
Joris Tammes <jorisezta@gmail.com>
2025-12-02 09:52:23 +0100

    testPayment

---
+++ testing/TestAcces.cs
@@ -0,0 +1,19 @@
+using Microsoft.Data.Sqlite;
+public abstract class TestAcces
+{
+    protected SqliteConnection _con;
+
+    public abstract string Table();
+
+    public TestAcces(string connectionstring = "Data Source=DataSources/TestDataBase.db")
+    {
+        _con = new SqliteConnection(connectionstring);
+        _con.Open();
+
+        using (var command = _con.CreateCommand())
+        {
+            command.CommandText = "PRAGMA foreign_keys = ON;";
+            command.ExecuteNonQuery();
+        }
+    }
+}
