9813ea0e7c416944333729b33822d38fb5e4a1ba
Joris Tammes <jorisezta@gmail.com>
2025-12-12 14:22:34 +0100

    idk

---
+++ unit-testing/SessionTest.cs
@@ -0,0 +1,16 @@
+// using Microsoft.AspNetCore.Mvc;
+// using Moq;
+// using Xunit;
+// using System.Collections.Generic; // added
+// using System.Linq;
+
+// public class Sessiontest
+// {
+//     public void TestCreateSession()
+//     {
+//         var sessionaccesMock = new Mock<ISessionAcces>();
+//         sessionaccesMock
+//             .Setup(u => u.AddSession(sessionkey, uid));
+
+//     }
+// }
